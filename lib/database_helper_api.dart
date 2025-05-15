68fe10f2670b5758b54d1f14b1f51f2d735b86de
João Pedro Cazarotti Batista <joaopedrocazarottijp@gmail.com>
2025-05-15 17:08:19 -0300

    começo da linkagem da api

---
+++ lib/database_helper_api.dart
@@ -0,0 +1,26 @@
+// import 'dart:convert';
+// import 'package:http/http.dart' as http;
+
+// class DatabaseHelper {
+//   static const String baseUrl = "http://10.56.46.48/apptarefasweb/public/api";
+
+//   //Create
+//   static Future<void> adicionarTarefa(String tarefa) async {
+    
+//   }
+
+//   //Read
+//   static Future<List<Map<String, dynamic>>> getTarefas() async {
+    
+//   }
+
+//   //Update
+//   static Future<void> editarTarefa(int id, String tarefa, int status) async {
+    
+//   }
+
+//   //Delete
+//   static Future<void> deletarTarefa(int id) async {
+    
+//   }
+// }
